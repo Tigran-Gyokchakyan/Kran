8be4e73073f8005c59008844afcf0647b09a9312
Tigran-Gyokchakyan <tigrangyokchakyan93@gmail.com>
2020-07-08 12:15:28 +0400

    project dont finished

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'u944822068_kran');
-define('DB_PASSWORD', 'kran2020');
-define('DB_NAME', 'u944822068_kran');
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
 
 
 
@@ -11,9 +11,9 @@ define('MIN_BONUS', 10);
 define('MAX_BONUS', 50);
 
 
-define('PAYEER_SECRET', 'pzD5Gg2hOVJNSIy0');
-define('PAYEER_ID', '510387173');
-define('PAYEER_ACCOUNT', 'P90594086');
+define('PAYEER_SECRET', '');
+define('PAYEER_ID', '');
+define('PAYEER_ACCOUNT', '');
 
 
 define('MIN_PAY', 1000);
